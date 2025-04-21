42c204ece79748799b7a6481ce16a2fd4b56ad11
Arloite <andrewdlee107@gmail.com>
2025-04-21 09:42:38 -0700

    Update ZooKeeperRunner.java
    4.5.6

--- ZooKeeperRunner.java
+++ ZooKeeperRunner.java
@@ -41,5 +41,23 @@ public class ZooKeeperRunner
     Owl owl2 = new Owl("mice,insects", true, 4.0);
     owl2.eat();
     hearTheAnimal(owl2);
+    //
+    Animal a = new Elephant("leaves, grasses, roots", false, 60.0);
+    a.isNocturnal();
+    //a.trumpet();
+    Animal a1 = new Animal();
+    a1.sleep();
+    //Object a2 = new Animal();
+    //a2.sleep();
+    Object o = new Object();
+    System.out.println(o.toString());
+    Elephant e = new Elephant("leaves, grasses, roots", false, 60.0);
+    System.out.println(e.toString());
+    Animal a3 = new Animal();
+    System.out.println(a3.toString());
+    Animal a4 = new Elephant("leaves, grasses, roots", false, 60.0);
+    System.out.println(a4.toString());
+    Object o2 = new Elephant("leaves, grasses, roots", false, 60.0);
+    System.out.println(o2.toString());
   }
 }
